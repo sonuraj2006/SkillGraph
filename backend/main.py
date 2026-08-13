bf133fba7a7b44de95809c38b60f5f7e33a4ed06
bmuk <21r21a1206@mlrintitutions.ac.in>
2026-08-13 22:12:55 +0530

    Allow deployed frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -14,6 +14,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://skillgraph-frontend-e67f.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
